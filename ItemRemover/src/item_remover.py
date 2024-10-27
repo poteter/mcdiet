b6762e98f54fb401b3e388d24389245bd9eb6794
Poteter <eith2312@gmail.com>
2024-10-27 14:59:29 +0100

    (ItemRemover) completed item remover, haven't run tests yet

--- ItemRemover/src/item_remover.py
+++ ItemRemover/src/item_remover.py
@@ -5,7 +5,29 @@ from dotenv import load_dotenv
 
 load_dotenv('environment/remover.env')
 
-def 
+def get_codes_from_queue(queue_name, rabbit_host, rabbit_port, rabbit_username, rabbit_password):
+    codes = []
+    credentials = pika.PlainCredentials(rabbit_username, rabbit_password)
+    parameters = pika.ConnectionParameters(
+        host=rabbit_host,
+        port=rabbit_port,
+        credentials=credentials
+    )
+
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue=queue_name, durable=True)
+
+    while True:
+        method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
+
+        if method_frame:
+            codes.append(body.decode('utf-8'))
+        else:
+            break
+
+    connection.close()
+    return codes # get_codes_from_queue
 
 def delete_items(codes, db_port):
     if not db_port:
@@ -21,3 +43,20 @@ def delete_items(codes, db_port):
             print(f"An error occurred while deleting code {code}: {e}")
 
     # delete_items
+
+def run():
+    db_port = os.getenv('DB_PORT')
+    non_carry_code_queue = os.getenv('NON_CARRY_CODES_QUEUE_NAME')
+
+    # rabbitMQ Configuration
+    rabbitmq_host = os.getenv('RABBITMQ_HOST', 'localhost')
+    rabbitmq_port = int(os.getenv('RABBITMQ_PORT', 5672))
+    rabbitmq_username = os.getenv('RABBITMQ_USERNAME', 'guest')
+    rabbitmq_password = os.getenv('RABBITMQ_PASSWORD', 'guest')
+
+    queue_codes = get_codes_from_queue(non_carry_code_queue, rabbitmq_host, rabbitmq_port, rabbitmq_username, rabbitmq_password)
+    delete_items(queue_codes, db_port)
+    # run
+
+if __name__ == '__main__':
+    run()
