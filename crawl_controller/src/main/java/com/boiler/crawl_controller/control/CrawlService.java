56f68d59b8f0783cc4fd78f9c3fa3039733ae235
Poteter <eith2312@gmail.com>
2024-10-31 11:28:19 +0100

    (crawl_constroller) working on crawl controller

--- crawl_controller/src/main/java/com/boiler/crawl_controller/control/CrawlService.java
+++ crawl_controller/src/main/java/com/boiler/crawl_controller/control/CrawlService.java
@@ -1,11 +1,21 @@
 package com.boiler.crawl_controller.control;
 
+import com.boiler.crawl_controller.rabbitmq_config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CrawlService {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public CrawlService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     public void runCrawl() {
-        
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, "run");
     }
 }
