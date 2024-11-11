58a239ed8034d27bd761911da26a56889f8ebe4b
Poteter <eith2312@gmail.com>
2024-11-11 14:20:29 +0100

    ( codekcal ) changed on_message handeling

--- codekcal/src/codesAndCals.py
+++ codekcal/src/codesAndCals.py
@@ -29,22 +29,23 @@ def convert_list_to_dict(lst):
         res_dict = {item['itemId']: {'energyKcal': item['energyKcal'], 'foodType': item['foodType']} for item in lst}
         return res_dict
     except Exception as e:
-        logging.error(f"Error when trying to convert list to dict : {e}")
+        logging.error(f"( convert_list_to_dict ) Error when trying to convert list to dict : {e}")
 
 ## get codes and kcal from db
 def get_codes_foodtype_and_calories_from_db(uri):
     try:
         response = requests.get(uri)
+        logging.info(f"( get_codes_foodtype_and_calories_from_db ) response code: {response.status_code}")
         response.raise_for_status()
 
         code_cal_foodtype_response = response.json()
         if isinstance(code_cal_foodtype_response, list):
             return code_cal_foodtype_response
         else:
-            print("Unexpected data format: Expected a list of codes.")
+            logging.info(f"( get_codes_foodtype_and_calories_from_db ) Unexpected data format: Expected a list of codes.")
             return []
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        logging.error(f"( get_codes_foodtype_and_calories_from_db ) An error occurred: {e}")
         return []
 
 ## send pairs to queue
@@ -59,28 +60,29 @@ def send_packet_to_queue(channel, codeKcal_queue_name, packet):
                 delivery_mode=2,
             )
         )
-        logging.info(f"Sent {message} to {codeKcal_queue_name}")
+        logging.info(f"( send_packet_to_queue ) Sent {message} to {codeKcal_queue_name}")
     except Exception as e:
-        logging.error(f"Error sending packet: {e}")
+        logging.error(f"( send_packet_to_queue ) Error sending packet: {e}")
 
 def graceful_shutdown(signal, frame):
     shutdown_flag.set()
-    logging.info(f"Signal {signal} received. Shutting down.")
+    logging.info(f"( graceful_shutdown ) Signal {signal} received. Shutting down.")
 
 ## makes data packet from param queue parameters and data from the database
 def process_message(body, codekcal_queue_name, gw_port, item_db_uri):
     try:
         params = json.loads(body.decode('utf-8'))
-        logging.info(f"Processing parameters: {params}")
+        logging.info(f"( process_message ) Processing parameters: {params}")
 
         # Fetch data from the database
         codes_foodtype_and_calories = get_codes_foodtype_and_calories_from_db(item_db_uri)
         if not codes_foodtype_and_calories:
-            logging.info("No data fetched from DB. Skipping packet creation.")
+            logging.info("( process_message ) No data fetched from DB. Skipping packet creation.")
             return
 
         # Convert list to dictionary
         code_cal_foodtype_dict = convert_list_to_dict(codes_foodtype_and_calories)
+        logging.info(f"( process_message ) Processing codes calories: {type(code_cal_foodtype_dict)}")
 
         # Combine data into a single packet
         packet = {**code_cal_foodtype_dict, **params}
@@ -94,16 +96,23 @@ def process_message(body, codekcal_queue_name, gw_port, item_db_uri):
         logging.error(f"Error processing message: {e}")
 
 ## makes and sends packet to codekcal queue
-def on_message(channel, method, properties, body, param, args):
-    param_queue_name, codekcal_queue_name, gw_port, item_db_uri = args
-    logging.info(f"Received message from queue '{param_queue_name}'")
+def on_message(channel, method, properties, body, param_queue_name, codekcal_queue_name, gw_port, item_db_uri):
+    logging.info(f"( on_message ) Received message from queue '{param_queue_name}'")
 
     packet = process_message(body, codekcal_queue_name, gw_port, item_db_uri)
     if packet:
         send_packet_to_queue(channel, codekcal_queue_name, packet)
 
+def create_on_message_callback(param_queue_name, codekcal_queue_name, gw_port, item_db_uri):
+    logging.info(f"( create_on_message_callback )")
+    def on_message_callback(ch, method, properties, body):
+        logging.info(f"( on_message_callback )")
+        on_message(ch, method, properties, body, param_queue_name, codekcal_queue_name, gw_port, item_db_uri)
+    return on_message_callback
+
+
 ## starts the rabbit channel as a separate thread and consumes the parameter queue
-def run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, rabbitmq_user, rabbitmq_password):
+def run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, rabbitmq_user, rabbitmq_password, rabbitmq_host):
     if not param_queue_name or not codekcal_queue_name or not gw_port or not item_db_uri:
         logging.error("One or more required environment variables are missing. Exiting.")
         sys.exit(1)
@@ -111,7 +120,7 @@ def run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, ra
     try:
         # Establish connection
         credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
-        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', credentials=credentials))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host, credentials=credentials))
         channel = connection.channel()
 
         # Declare the parameter queue
@@ -120,13 +129,11 @@ def run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, ra
         # Define the callback with additional arguments
         channel.basic_consume(
             queue=param_queue_name,
-            on_message_callback=lambda ch, method, properties, body, args: on_message(
-                ch, method, properties, body, args, (param_queue_name, codekcal_queue_name, gw_port, item_db_uri)
-            ),
+            on_message_callback=create_on_message_callback(param_queue_name, codekcal_queue_name, gw_port, item_db_uri),
             auto_ack=True
         )
 
-        logging.info(f"waiting for message from {param_queue_name}")
+        logging.info(f"( run_consumer ) waiting for message from {param_queue_name}")
 
         # Start consuming in a separate thread to allow graceful shutdown
         consume_thread = threading.Thread(target=channel.start_consuming)
@@ -138,11 +145,11 @@ def run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, ra
         channel.stop_consuming()
         consume_thread.join()
         connection.close()
-        logging.info("RabbitMQ consumer has been shut down gracefully.")
+        logging.info(f"( run_consumer ) RabbitMQ consumer has been shut down gracefully.")
     except pika.exceptions.AMQPConnectionError as e:
-        logging.error(f"AMQP connection error: {e}")
+        logging.error(f"( run_consumer ) AMQP connection error: {e}")
     except Exception as e:
-        logging.error(f"Unexpected error: {e}")
+        logging.error(f"( run_consumer ) Unexpected error: {e}")
 
 def run():
     path_flag_docker = True
@@ -153,18 +160,26 @@ def run():
 
     param_queue_name = os.getenv("PARAM_QUEUE_NAME")
     codekcal_queue_name = os.getenv("CODEKCAL_QUEUE_NAME")
-    item_db_uri = f'http://localhost:{os.getenv("GW_PORT")}/itemController/api/item/codecal'
+
+    if path_flag_docker:
+        gateway_host_name = "gateway"
+    else:
+        gateway_host_name = "localhost"
+
+    item_db_uri = f'http://{gateway_host_name}:{os.getenv("GW_PORT")}/item_db/api/item/codecal'
+    logging.info(f"( run ) item_db_uri: {item_db_uri}")
     gw_port = os.getenv("GW_PORT")
     rabbitmq_user = os.getenv('RABBITMQ_USER')
     rabbitmq_password = os.getenv('RABBITMQ_PASS')
+    rabbitmq_host = os.getenv('RABBITMQ_HOST')
 
     # signal handlers
     signal.signal(signal.SIGINT, graceful_shutdown)
     signal.signal(signal.SIGTERM, graceful_shutdown)
 
-    logging.info("Starting RabbitMQ consumer.")
-    run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, rabbitmq_user, rabbitmq_password)
-    logging.info("Consumer stopped.")
+    logging.info("( run ) Starting RabbitMQ consumer.")
+    run_consumer(param_queue_name, codekcal_queue_name, gw_port, item_db_uri, rabbitmq_user, rabbitmq_password, rabbitmq_host)
+    logging.info("( run ) Consumer stopped.")
 
 if __name__ == '__main__':
     run()
